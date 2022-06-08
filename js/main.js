dbc013d48445ec7237ebab1763c88d7acecf3bcb
Danq Nhat Huy <94223877+dnh273@users.noreply.github.com>
2022-06-08 09:41:27 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,10 +5,4 @@ themeButton.addEventListener('click', () => {
     document.querySelector('body').classList.toggle('lightMode');
 })
 
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-})
 
