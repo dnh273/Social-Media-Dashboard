ddf31360f414f1b0fcb3cf21601872d8d33d3500
dnh2703 <dnh2703@gmail.com>
2022-06-08 09:54:29 +0700

    update 1.1

--- js/main.js
+++ js/main.js
@@ -5,4 +5,18 @@ themeButton.addEventListener('click', () => {
     document.querySelector('body').classList.toggle('lightMode');
 })
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
 
+sr.reveal(`.header__title`)
+sr.reveal(`.header__subtitle`, { delay: 500 })
+sr.reveal(`.darkMode`, { delay: 600 })
+sr.reveal(`.dashboard`, { delay: 700 })
+sr.reveal(`.overview__title`, { interval: 100, delay: 700 })
+sr.reveal(`.overview`, { delay: 800 })
+sr.reveal(`.attribution`, { delay: 900 })
+sr.reveal(`.filters`, { delay: 1000 })
